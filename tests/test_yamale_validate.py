f18ce44f5795da12e638f056df2499dead6a2088
Cenk Kuecuek <kuecuek@sipgate.de>
2020-09-10 11:53:14 +0200

    reformat code

--- tests/test_yamale_validate.py
+++ tests/test_yamale_validate.py
@@ -10,23 +10,24 @@ def test_import_module_works():
 def test_valid_yaml(module_args):
     import yamale_validate
 
-    set_module_args( {
+    set_module_args({
         **module_args,
         "data_path": "tests/data_good.yaml"
-     })
+    })
 
     with raises(AnsibleExitJson) as result:
         yamale_validate.main()
 
     assert (result.value.args[0]['changed'] is False)
 
+
 def test_invalid_yaml(module_args):
     import yamale_validate
 
-    set_module_args( {
+    set_module_args({
         **module_args,
         "data_path": "tests/data_bad.yaml"
-     })
+    })
 
     with raises(AnsibleFailJson) as result:
         yamale_validate.main()
